e383cd39c57a4899326f519f7295d6d84364770b
Pratik <pratikpatil12324@gmail.com>
2025-12-22 17:29:07 +0530

    modified API

---
+++ BACKEND/Server/routes/student.js
@@ -0,0 +1,115 @@
+const express=require("express");
+const result=require("../utils/result")
+const pool=require("../db/pool")
+const crypto=require("crypto-js")
+
+const router=express.Router();
+//add a student to a course
+router.post("/register-to-course",(req,res)=>{
+    const {name,email, course_id,mobile_no}=req.body;
+
+    // step 1 check if user exists
+    const usersql="SELECT * FROM users WHERE email=?";
+    pool.query(usersql,[email],(error,data)=>{
+        if(error){
+            return res.send(result.createResult(error));
+        }
+        else if(data.length==0){
+            const password = "sunbeam";
+            // crypto 
+            const hashedpassword=crypto.SHA256(password).toString();
+            const role="student";
+             const uusersql=`INSERT INTO users(email,password,role) VALUES(?,?,?)`;
+             pool.query(uusersql,[email,hashedpassword,role],(error,data)=>{
+
+                if(error){
+                    return res.send(result.createResult(error));
+                }
+
+                const sql=`INSERT INTO students(name,email, course_id,mobile_no) VALUES(?,?,?,?)`;
+                 pool.query(sql,[name,email, course_id,mobile_no],(error,data)=>{
+                     if(error){
+                        return res.send(result.createResult(error));
+                    }
+                  res.send(result.createResult(null,data));
+                })
+
+
+                
+             })
+
+
+            
+        }
+         else{
+             const sql=`INSERT INTO students(name,email, course_id,mobile_no) VALUES(?,?,?,?)`;
+             pool.query(sql,[name,email, course_id,mobile_no],(error,data)=>{
+             if(error){
+                 return res.send(result.createResult(error));
+             }
+            res.send(result.createResult(null,data));
+            })
+    
+        }
+        
+})
+
+})
+
+   
+  
+
+
+//update password
+router.put("/changepassword",(req,res)=>{
+    const{email,newpassword,confirmpassword}=req.body;
+   
+    
+    
+     if(newpassword != confirmpassword){
+        return res.send(result.createResult("passwords do not match"));
+    }
+
+    const hashedpassword=crypto.SHA256(newpassword).toString();
+    const sql=`UPDATE users SET password=? WHERE email=?`;
+    pool.query(sql,[hashedpassword,email],(error,data)=>{
+        if(error){
+            return res.send(result.createResult(error));
+        }
+        
+        else if(data.affectedRows==0){
+            return res.send("Invalid credintial");
+        }
+
+        res.send(result.createResult(null,data));
+    })
+
+})
+
+// /get all registered courses of a student
+
+router.get("/my-courses",(req,res)=>{
+    const { email } = req.query;
+
+    const sql = `
+      SELECT c.course_name
+      FROM courses c
+      INNER JOIN students s ON s.course_id = c.course_id
+      WHERE s.email = ?
+    `;
+
+    pool.query(sql,[email],(error,data)=>{
+        if(error){
+            return res.send(result.createResult(error));
+        }
+        else if(data.length==0){
+             return res.send(result.createResult("NO courses are available"));
+        }
+        res.send(result.createResult(null,data));
+    })
+})
+
+
+
+
+module.exports=router;
--- BACKEND/Server/server.js
+++ BACKEND/Server/server.js
@@ -6,9 +6,11 @@ const app=express();
 
 const commenApi=require("./routes/common_Api");
 const adminRouter=require("./routes/Admin");
+const studentRouter=require("./routes/student");
 app.use(express.json())
 app.use("/common",commenApi);
 app.use("/admin",adminRouter);
+app.use("/student",studentRouter);
 
 app.listen(4000,"localhost",()=>{
     console.log("server start at port 4000");
